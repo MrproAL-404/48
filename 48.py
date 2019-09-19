d85b11003558a27f1ca5fd1b13d9c42ccb57a643
Mr. ProAL <53925760+MrproAL-404@users.noreply.github.com>
2019-09-19 08:36:39 +0700

    Create 48.py

---
+++ 48.py
@@ -0,0 +1,40 @@
+# Mr ProAL
+# Indonesia Teror Family
+
+from requests import Session
+import re, sys
+s = Session()
+
+try:
+	print("\n\n * SMS Gratis by Mr ProAL - Indonesia Teror Family\n * Gunakan kode negara elu bred(ex: 628xxxxx)\n")
+	no = int(input(" Nomor ente : "))
+	msg = input(" Pesannye bro : ")
+except:
+	print("\n\t* Cek nomer and pesan lu bred! *")
+	sys.exit()
+
+headers = {
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 4.4.2; Nexus 4 Build/KOT49H) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.114 Mobile Safari/537.36',
+    'Referer': 'http://sms.payuterus.biz/alpha/'
+}
+
+bypass = s.get("http://sms.payuterus.biz/alpha/?a=keluar", headers=headers).text
+key = re.findall(r'value="(\d+)"', bypass)[0]
+jml = re.findall(r'<span>(.*?) = </span>', bypass)[0]
+captcha = eval(jml.replace("x", "*").replace(":", "/"))
+
+data = {
+	'nohp':no,
+	'pesan':msg,
+	'captcha':captcha,
+	'key':key
+}
+
+send = s.post("http://sms.payuterus.biz/alpha/send.php", headers=headers, data=data).text
+
+if 'SMS Gratis Geus Dikirim Bred' in send:
+	print(f"\n  Anjay Pengiriman sukses ]\n  [ {no} : {msg} ]\n")
+elif 'SORRY....!' in send:
+	print("\n  [ Santay bred tunggu 15 menit untuk mengirim pesan yg sama ]\n")
+else:
+	print("\n  [ Oalahh Pengiriman lu gagal ]\n")
